8ed8afffc9da1c6ad7e12bccf445da0ce46cc942
Leonardo Saippa <leonardomsaippa@gmail.com>
2021-04-06 16:58:12 -0300

    Initial Commit

---
+++ Virtual Tourist/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Virtual Tourist
+//
+//  Created by Leonardo Saippa on 06/04/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
