df787414dec3f546adc2a6c516f65c44468990d3
Leonardo Saippa <leonardomsaippa@gmail.com>
2021-04-14 18:49:10 -0300

    Add handler parameter to show alert

--- Virtual Tourist/View/ViewExtension.swift
+++ Virtual Tourist/View/ViewExtension.swift
@@ -10,9 +10,9 @@ import UIKit
 
 extension UIViewController {
 
-    func showAlert(message: String, title: String) {
+    func showAlert(message: String, title: String, handler:((UIAlertAction) -> Void)?) {
         let alertVC = UIAlertController(title: title, message: message, preferredStyle: .alert)
-        alertVC.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        alertVC.addAction(UIAlertAction(title: "OK", style: .default, handler: handler))
         present(alertVC, animated: true)
     }
 
